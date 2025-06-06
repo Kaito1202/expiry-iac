dd885b830c762993d2b7a8782c46cbf8a67db05e
Kaito1202 <127775143+Kaito1202@users.noreply.github.com>
2025-06-06 19:02:29 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -22,8 +22,7 @@ module "eks" {
   cluster_name    = "expiry-eks"
   cluster_version = "1.32"
   subnet_ids      = module.vpc.private_subnets
-  vpc_id          = module.vpc.vpc_id
-  kms_key_aliases = ["eks/expiry-eks-v2"] 
+  vpc_id          = module.vpc.vpc_id 
   eks_managed_node_groups = {
     default = {
       desired_size = 2
